666e4b920976775986d726aef82fa0e3adf939ca
gBhatt1998 <gauravbhatt1998@gmail.com>
2025-06-02 21:30:58 +0530

    first commit

---
+++ src/test/java/com/example/hotelManagementBackend/HotelManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hotelManagementBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
